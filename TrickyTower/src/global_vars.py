dcb7add85701eeda3ef29ff7ca31de52e37851b5
CR0ZER <109722353+CR0ZER@users.noreply.github.com>
2023-01-12 23:34:37 +0100

    add

--- TrickyTower/src/global_vars.py
+++ TrickyTower/src/global_vars.py
@@ -23,8 +23,11 @@ height = 1600
 width = 900
 background_menu = "./src/assets/background_menu.png"
 background_game = "./src/assets/background_game.png"
-square = "./src/assets/Game_Assets/R/R.png"
-Sshape = "./src/assets/Game_Assets/S/S.png"
-Lshape = "./src/assets/Game_Assets/L/L.png"
-Tshape = "./src/assets/Game_Assets/T/T_2.png"
-IShape = "./src/assets/Game_Assets/I/1.png"
+
+square = "./src/assets/Game_Assets/R.png"
+Sshape = "./src/assets/Game_Assets/S.png"
+Sshape_bis = "./src/assets/Game_Assets/S_bis.png"
+Lshape = "./src/assets/Game_Assets/L.png"
+Lshape_bis = "./src/assets/Game_Assets/L_bis.png"
+Tshape = "./src/assets/Game_Assets/T_2.png"
+IShape = "./src/assets/Game_Assets/I.png"
